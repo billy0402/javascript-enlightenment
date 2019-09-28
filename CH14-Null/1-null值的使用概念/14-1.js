a8886cf8b7466fd5d7f9ffda5fbbc62c95247b69
Billy Huang <10646003@ntub.edu.tw>
2019-09-28 12:00:45 +0800

    CH14 - Null

---
+++ CH14-Null/1-null值的使用概念/14-1.js
@@ -0,0 +1,5 @@
+// 屬性 foo 正在等待一個值，所以我們將它的初始值設為 null
+var myObjectObject = { foo: null };
+
+console.log(myObjectObject.foo);
+// logs 'null'
---
+++ CH14-Null/2-typeof會將null值回傳為object/14-2.js
@@ -0,0 +1,4 @@
+var myObject = null;
+
+console.log(typeof myObject); // logs 'object'，不是很有幫助
+console.log(myObject === null); // logs true，只有真正的 null 值才會如此
