030827e24b7e50e7b22ce5017c24f18f6bae6703
Billy Huang <10646003@ntub.edu.tw>
2019-09-28 12:01:04 +0800

    CH16 - Math功能

---
+++ CH16-Math功能/2-Math屬性與方法/16-1.js
@@ -0,0 +1,29 @@
+// 屬性
+Math.E;
+Math.LN2;
+Math.LN10;
+Math.LOG2E;
+Math.LOG10E;
+Math.PI;
+Math.SQRT1_2;
+Math.SQRT2;
+
+// 方法
+Math.abs();
+Math.acos();
+Math.asin();
+Math.atan();
+Math.atan2();
+Math.ceil();
+Math.cos();
+Math.exp();
+Math.floor();
+Math.log();
+Math.max();
+Math.min();
+Math.pow();
+Math.random();
+Math.round();
+Math.sin();
+Math.sort();
+Math.tan();
